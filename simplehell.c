aadf434f1d7304c8cb44a8afb312a3c8de58a06c
Hares Mahmood <hmahmood229@gmail.com>
2020-01-26 16:40:16 +0000

    Add support for tokenizing user input.
    Upon receiving user input, shell simply prints every token on a new line.

--- simplehell.c
+++ simplehell.c
@@ -1,6 +1,31 @@
-#include <stdio.h>
+#include <stdio.h> 
+#include <string.h>
+
+/* boolean values */
+#define true 0
+#define false 1
+typedef int bool;
+
+#define MAX_INPUT 512
+static const char prompt[] = "> ";
 
 int main() 
-{   
+{ 
+    bool terminated = false;
+    
+    while (!terminated)
+    {
+        char input[MAX_INPUT]; 
+        printf(prompt); 
+        fgets(input, MAX_INPUT, stdin); 
+        
+        char* token = strtok(input, " ");
+        while (token) 
+        {
+            printf("%s\n", token);
+            token = strtok(NULL, " ");
+        }
+    }
+    
     return 0;
 }
