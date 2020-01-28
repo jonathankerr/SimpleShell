95c761d7d20785e0e3d34abf48c2d8ea02e4bdc8
Matthew King <matthewmk108@gmail.com>
2020-01-28 15:15:55 +0000

    Update simpleshell.h

--- simpleshell.h
+++ simpleshell.h
@@ -5,6 +5,10 @@ typedef int bool;
 
 /* Maximum number of characters user input can contain */
 #define MAX_INPUT 512 
+
 /* String that will be printed every time user is able to enter a command */
 static const char prompt[] = "> ";
+
+
+/* Function declarations */
 char *strtok(char *str, const char *delim);
