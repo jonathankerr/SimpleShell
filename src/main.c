7db9e8e67256eeed60db812540c9b12bebd82325
James Boner <james.boner.2018@uni.strath.ac.uk>
2020-02-24 23:31:59 +0000

    removed an exit shell call to stop double exit msg

--- src/main.c
+++ src/main.c
@@ -52,7 +52,7 @@ int main()
 	
 		// Closes program if exit is typed or if Ctrl-D is pressed
 		// Also sets the current working directory to the initial working directory
-		terminated = exitShell(tokens[0], (strcmp(input, "exit") == 0 || feof(stdin)), initDir);
+		//terminated = exitShell(tokens[0], (strcmp(input, "exit") == 0 || feof(stdin)), initDir);
     }
 
 	return 0;
