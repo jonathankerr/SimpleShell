8be65d2ae91a040a8e73715a3dd9f24443ddcdab
Justin Leung SE2018 <gwb18170@muir-19.cis.strath.ac.uk>
2020-03-03 11:48:52 +0000

    Add printing and setting path during exit.

--- src/simpleshell.h
+++ src/simpleshell.h
@@ -60,7 +60,7 @@ void printFullArray(char array[][MAX_USERINPUT], int maxSize);
 void emptyArray(char** tokens, int maxSize, int maxUserInput);
 void tokenize(char tokens[50][512], char* input);
 int parseInput(char tokens[50][512], char history[MAX_HISTORY_SIZE][MAX_USERINPUT]);
-bool exitShell(char* input, bool shellStatus, char* dir);
+bool exitShell(char* input, bool shellStatus, char* dir, char* path);
 char* getCWD();
 void addHistory(char *input, char history[MAX_HISTORY_SIZE][MAX_USERINPUT]);
 void chomp(char *s);
