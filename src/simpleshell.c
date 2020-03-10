045db3450795f84318f91e8b1d6c9ae727d35830
haresm <hmahmood229@gmail.com>
2020-03-10 11:43:03 +0000

    Fix errors in "invokeHistory".

--- src/simpleshell.c
+++ src/simpleshell.c
@@ -324,6 +324,10 @@ void invokeHistory(char history[MAX_HISTORY_SIZE][MAX_USERINPUT], char* token)
 
 		index--;	
 	}
+	else if (token[2] != '\0' && token[2] != '!')
+	{
+		printf("Invalid input.\nPlease make sure to use one of the following format: !<index>, or !!\n");
+	}
 
 	if (historyCount(history) > 0)
 	{
@@ -338,7 +342,7 @@ void invokeHistory(char history[MAX_HISTORY_SIZE][MAX_USERINPUT], char* token)
 	}
 	else
 	{
-		printf("No commands in history.\n");
+		printf("No commands in history.\n\n");
 	}
 }
 
